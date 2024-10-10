8fa69cbc518809a9cab1402d3e040fdc4b1765e6
Mariano J Correa <marianocn98@gmail.com>
2024-10-09 20:31:04 -0500

    updates to landing page

--- haku-app/src/landingPage.tsx
+++ haku-app/src/landingPage.tsx
@@ -1,11 +1,10 @@
-//import "./landingPage.css";
+import "./landingPage.module.css";
 import landingStyles from "./landingPage.module.css";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FooterComponent from "./footerComp";
 import Navbar from "./Navbar";
 
-// script tag in HTML loads particlesJS to the global window object
 declare global {
   interface Window {
     particlesJS: any;
@@ -20,51 +19,57 @@ function LandingPage() {
   };
 
   useEffect(() => {
-    // Text animation:
-    const texts = document.querySelectorAll(".fade-in"); // querySelectorAll returns a node list, which is array like.
+    // Text animation
+    const texts = document.querySelectorAll(".fade-in");
     texts.forEach((text, index) => {
-      // each text is displayed 1 second after the other
-      (text as HTMLElement).style.animationDelay = `${index * 1}s`; // animation delay is a css property
+      (text as HTMLElement).style.animationDelay = `${index * 1}s`;
     });
 
-    // Initialize Particles Animation:
+    // Initialize Particles Animation for multiple containers
     if (window.particlesJS) {
-      window.particlesJS("particles-js", {
-        particles: {
-          number: { value: 80, density: { enable: true, value_area: 800 } },
-          color: { value: "#ffffff" },
-          opacity: { value: 0.5, random: false },
-          size: { value: 3, random: true },
-          line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1,
+      [
+        "particles-js-1",
+        "particles-js-2",
+        "particles-js-3",
+        "particles-js-4",
+      ].forEach((containerId) => {
+        window.particlesJS(containerId, {
+          particles: {
+            number: { value: 80, density: { enable: true, value_area: 800 } },
+            color: { value: "#ffffff" },
+            opacity: { value: 0.5, random: false },
+            size: { value: 3, random: true },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+            },
           },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            bounce: false,
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: { enable: true, mode: "repulse" },
+              onclick: { enable: true, mode: "push" },
+              resize: true,
+            },
+            modes: {
+              repulse: { distance: 100, duration: 0.4 },
+              push: { particles_nb: 4 },
+            },
           },
-        },
-        interactivity: {
-          detect_on: "canvas",
-          events: {
-            onhover: { enable: true, mode: "repulse" },
-            onclick: { enable: true, mode: "push" },
-            resize: true,
-          },
-          modes: {
-            repulse: { distance: 100, duration: 0.4 },
-            push: { particles_nb: 4 },
-          },
-        },
-        retina_detect: true,
+          retina_detect: true,
+        });
       });
     }
   }, []);
@@ -72,7 +77,8 @@ function LandingPage() {
   return (
     <>
       <Navbar />
-      <div className={landingStyles.landingcontainer}>
+
+      <div className={landingStyles.sectionContainer}>
         <div className={landingStyles.landingPageContent}>
           <p className="fade-in" id={landingStyles.introText}>
             HAKU
@@ -80,21 +86,46 @@ function LandingPage() {
           <p className="fade-in">No somos una plataforma de inversion.</p>
           <p className="fade-in">Somos un buscador de fondos.</p>
           <p className="fade-in">Para que encuentres a donde invertir.</p>
+        </div>
+        <div
+          id="particles-js-1"
+          className={landingStyles.particlesContainer}
+        ></div>
+      </div>
+
+      <div className={landingStyles.sectionContainer}>
+        <div className={landingStyles.landingPageContent}>
+          <h1>20-Year Historical Data </h1>
+          <div id={landingStyles.landingPage2Fondos}>
+            <h2>Fondos Mutuos - </h2>
+            <h2>ETFS -</h2>
+            <h2>Fondos de Inversion</h2>
+          </div>
+        </div>
+        <div
+          id="particles-js-2"
+          className={landingStyles.particlesContainer}
+        ></div>
+      </div>
+
+      <div className={landingStyles.sectionContainer}>
+        <div className={landingStyles.landingPageContent}>
+          <h1>Explora Fondos</h1>
           <div className={landingStyles.buttonsContainer}>
             <button
-              className={`${landingStyles.exploreButton} fade-in`}
+              className={landingStyles.exploreButton}
               onClick={handleFondosMutuosClick}
             >
               Fondos Mutuos
             </button>
             <button
-              className={`${landingStyles.exploreButton} fade-in`}
+              className={landingStyles.exploreButton}
               onClick={() => navigate("/etf")}
             >
               ETFs
             </button>
             <button
-              className={`${landingStyles.exploreButton} fade-in`}
+              className={landingStyles.exploreButton}
               onClick={() => navigate("/fondos-de-inversion")}
             >
               Fondos de Inversion
@@ -102,18 +133,23 @@ function LandingPage() {
           </div>
         </div>
         <div
-          id="particles-js"
+          id="particles-js-3"
           className={landingStyles.particlesContainer}
         ></div>
       </div>
-      <div className={landingStyles.landingpage2}>
-        <p>
-          Encuentra todos los Fondos Mutuos, ETFS y Fondos de Inversión del Peru
-        </p>
-        <p>Explora Alternativas</p>
-        <p>Simula tu Inversión</p>
-        <p>Descarga Datos</p>
+
+      <div className={landingStyles.sectionContainer}>
+        <div className={landingStyles.landingPageContent}>
+          <h1>Descarga Datos</h1>
+          <h1>&</h1>
+          <h1>Arma tu Portafolio</h1>
+        </div>
+        <div
+          id="particles-js-4"
+          className={landingStyles.particlesContainer}
+        ></div>
       </div>
+
       <FooterComponent />
     </>
   );
